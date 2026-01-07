d5df4c3f89ae0ef1cf4ab8be8002fbb639bd148a
Riyanghouri786 <riyanghouri66@gmail.com>
2026-01-07 03:01:12 -0800

    SeamlessIntegrations svh and png added

--- src/app/components/SeamlessIntegrations.jsx
+++ src/app/components/SeamlessIntegrations.jsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 
 const integrations = [
-  { name: "Adobe", src: "/logos/adobe.svg" },
-  { name: "Google Cloud", src: "/logos/google-cloud.svg" },
-  { name: "Google Partner", src: "/logos/google.svg" },
-  { name: "PayPal", src: "/logos/paypal.svg" },
-  { name: "Odoo", src: "/logos/odoo.svg" },
-  { name: "Entrepreneur", src: "/logos/entrepreneur.svg" },
-  { name: "Stripe", src: "/logos/stripe.svg" },
-  { name: "Magento", src: "/logos/magento.svg" },
+  { name: "Adobe", src: "/adobe.svg" },
+  { name: "Google Cloud", src: "/google-cloud.svg" },
+  { name: "Google Partner", src: "/google.svg" },
+  { name: "PayPal", src: "/paypal.svg" },
+  { name: "Odoo", src: "/odoo.png" },
+  { name: "Entrepreneur", src: "/Entrepreneur.png" },
+  { name: "Stripe", src: "/strip.svg" },
+  { name: "Magento", src: "/magento.svg" },
 ];
+
 
 export default function SeamlessIntegrations() {
   return (
-    <section className="bg-white py-20 px-4">
+    <section className="bg-gray-100 py-20 px-4">
       <div className="max-w-6xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
           Seamless Integrations
@@ -29,7 +30,7 @@ export default function SeamlessIntegrations() {
           {integrations.map((item, index) => (
             <div
               key={index}
-              className="flex items-center justify-center rounded-2xl bg-white p-6
+              className="flex items-center justify-center cursor-pointer rounded-2xl gap-4 bg-white p-6
                          shadow-[0_10px_30px_rgba(0,0,0,0.06)]
                          transition-all duration-300
                          hover:-translate-y-1.5
@@ -38,10 +39,15 @@ export default function SeamlessIntegrations() {
               <Image
                 src={item.src}
                 alt={item.name}
-                width={120}
-                height={50}
-                className="object-contain"
+                width={item.name === "Entrepreneur" || item.name === "Odoo" || item.name === "Stripe" ? 80 : 50}
+                height={item.name === "Entrepreneur" || item.name === "Odoo" || item.name === "Stripe" ? 80 : 50}
+                className="object-contain rounded-xl"
               />
+
+              <p className="text-xl font-medium">
+                {item.name === "Entrepreneur" || item.name === "Odoo" || item.name === "Stripe" ? "" : item.name}
+              </p>
+
             </div>
           ))}
         </div>
